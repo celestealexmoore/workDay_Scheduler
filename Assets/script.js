4f19e5d613dabf1351c556b368632cbd5f266e7c
Celeste Moore <celestealexmoore@gmail.com>
2022-05-25 15:40:08 -0500

    colors change for past, present and future

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,47 @@
 const currentDay = document.getElementById("currentDay");
 const timeblockParent = document.getElementById("timeblockParent");
-let blockTime = 9;
+let blockTime = 4;
+const currentTime = moment().format("h");
+console.log(currentTime);
 
-currentDay.innerHTML = moment().format("MMMM Do, YYYY");
+currentDay.innerHTML = moment().format("MMMM Do, YYYY, h:mm a");
 
 for (let i = 0; i < 8; i++) {
   const timeblockRow = document.createElement("div");
   const timeblockHr = document.createElement("div");
   const timeblockText = document.createElement("input");
-  const saveBtnParent = document.createElement("i");
-
+  const saveBtn = document.createElement("i");
   // creates unique identifier for each timeblock
   timeblockRow.setAttribute("id", "hour-" + i);
-
   timeblockRow.setAttribute("class", "row time-block");
   timeblockHr.setAttribute("class", "col hour");
   timeblockText.setAttribute("class", "col-8 text form-control");
   timeblockText.setAttribute("type", "text");
-  saveBtnParent.setAttribute(
+  saveBtn.setAttribute(
     "class",
     "col saveBtn bi bi-cloud-download-fill fa-2x align-bottom"
   );
-  // creates unique identifier for each timeblock
-  saveBtnParent.setAttribute("id", "save-" + i);
+  // creates unique identifier for each saveBtn
+  saveBtn.setAttribute("id", "save-" + i);
 
-  timeblockHr.innerText = blockTime++ + "am";
+  blockTime += 1;
 
-  if (blockTime > 9) {
+  timeblockHr.innerText = blockTime + "am";
+
+  if (blockTime == currentTime) {
+    timeblockText.setAttribute("class", "present col-8 text");
+  } else if (blockTime < currentTime) {
     timeblockText.setAttribute("class", "past col-8 text");
-  }
+  } else if (blockTime > currentTime) {
+    timeblockText.setAttribute("class", "future col-8 text");
+  };
 
   timeblockParent.appendChild(timeblockRow);
   timeblockRow.appendChild(timeblockHr);
   timeblockRow.appendChild(timeblockText);
-  timeblockRow.appendChild(saveBtnParent);
+  timeblockRow.appendChild(saveBtn);
 
-  saveBtnParent.addEventListener("click", function () {
+  saveBtn.addEventListener("click", function () {
     localStorage.setItem(timeblockRow.id, timeblockText.value);
   });
 
@@ -43,13 +49,9 @@ for (let i = 0; i < 8; i++) {
     timeblockRow.id,
     timeblockText.value
   );
+
+  //   if(blockTime > 11) {
+  //     let hr12 = document.getElementById('hour-7');
+  //     hr12.innerText = blockTime + 'pm';
+  // }
 }
-
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
