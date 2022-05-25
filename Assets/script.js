5bcc78faa97e202c3eea812860a52aae6f4033d9
Celeste Moore <celestealexmoore@gmail.com>
2022-05-25 13:34:09 -0500

    events save to local storage and persist when refreshed.

--- Assets/script.js
+++ Assets/script.js
@@ -10,6 +10,9 @@ for (let i = 0; i < 8; i++) {
   const timeblockText = document.createElement("input");
   const saveBtnParent = document.createElement("i");
 
+  // creates unique identifier for each timeblock
+  timeblockRow.setAttribute("id", "hour-" + i);
+
   timeblockRow.setAttribute("class", "row time-block");
   timeblockHr.setAttribute("class", "col hour");
   timeblockText.setAttribute("class", "col-8 text form-control");
@@ -18,24 +21,34 @@ for (let i = 0; i < 8; i++) {
     "class",
     "col saveBtn bi bi-cloud-download-fill fa-2x align-bottom"
   );
+  // creates unique identifier for each timeblock
+  saveBtnParent.setAttribute("id", "save-" + i);
 
   timeblockHr.innerText = blockTime++ + "am";
 
-  if(blockTime > 9) {
-      timeblockText.setAttribute('class', 'past col-8 text');
+  if (blockTime > 9) {
+    timeblockText.setAttribute("class", "past col-8 text");
   }
 
   timeblockParent.appendChild(timeblockRow);
   timeblockRow.appendChild(timeblockHr);
   timeblockRow.appendChild(timeblockText);
   timeblockRow.appendChild(saveBtnParent);
+
+  saveBtnParent.addEventListener("click", function () {
+    localStorage.setItem(timeblockRow.id, timeblockText.value);
+  });
+
+  timeblockText.value = localStorage.getItem(
+    timeblockRow.id,
+    timeblockText.value
+  );
 }
+
 
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
